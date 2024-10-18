ff1efa54a2846083979a1e6eaeaf1cfd2621cff9
guilhemtrd <guilhem.trd@gmail.com>
2024-10-18 11:40:25 +0200

    ✨ Add User entity with API resource and time tracking fields

---
+++ src/Entity/User.php
@@ -0,0 +1,219 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\UserRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
+#[ApiResource]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $username = null;
+
+    #[ORM\Column(length: 180)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $password = null;
+
+    #[ORM\Column(length: 100, nullable: true)]
+    private ?string $full_name = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $image_profile = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $palmares = null;
+
+    #[ORM\Column(type: Types::TIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $time_5k = null;
+
+    #[ORM\Column(type: Types::TIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $time_10k = null;
+
+    #[ORM\Column(type: Types::TIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $time_semi = null;
+
+    #[ORM\Column(type: Types::TIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $time_marathon = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $created_at = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $updated_at = null;
+
+    #[ORM\Column]
+    private ?bool $status = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): static
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getFullName(): ?string
+    {
+        return $this->full_name;
+    }
+
+    public function setFullName(?string $full_name): static
+    {
+        $this->full_name = $full_name;
+
+        return $this;
+    }
+
+    public function getImageProfile(): ?string
+    {
+        return $this->image_profile;
+    }
+
+    public function setImageProfile(?string $image_profile): static
+    {
+        $this->image_profile = $image_profile;
+
+        return $this;
+    }
+
+    public function getPalmares(): ?string
+    {
+        return $this->palmares;
+    }
+
+    public function setPalmares(?string $palmares): static
+    {
+        $this->palmares = $palmares;
+
+        return $this;
+    }
+
+    public function getTime5k(): ?\DateTimeInterface
+    {
+        return $this->time_5k;
+    }
+
+    public function setTime5k(?\DateTimeInterface $time_5k): static
+    {
+        $this->time_5k = $time_5k;
+
+        return $this;
+    }
+
+    public function getTime10k(): ?\DateTimeInterface
+    {
+        return $this->time_10k;
+    }
+
+    public function setTime10k(?\DateTimeInterface $time_10k): static
+    {
+        $this->time_10k = $time_10k;
+
+        return $this;
+    }
+
+    public function getTimeSemi(): ?\DateTimeInterface
+    {
+        return $this->time_semi;
+    }
+
+    public function setTimeSemi(?\DateTimeInterface $time_semi): static
+    {
+        $this->time_semi = $time_semi;
+
+        return $this;
+    }
+
+    public function getTimeMarathon(): ?\DateTimeInterface
+    {
+        return $this->time_marathon;
+    }
+
+    public function setTimeMarathon(?\DateTimeInterface $time_marathon): static
+    {
+        $this->time_marathon = $time_marathon;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): static
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(?\DateTimeInterface $updated_at): static
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+
+    public function isStatus(): ?bool
+    {
+        return $this->status;
+    }
+
+    public function setStatus(bool $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
