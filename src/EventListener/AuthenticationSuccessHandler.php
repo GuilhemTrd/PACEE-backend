fa8ebd656a5164f3b6008debcea218ca1d4f9775
guilhemtrd <guilhem.trd@gmail.com>
2024-10-23 21:30:05 +0200

    ✨ Add user ID and profile details to login response with JWT authentication

---
+++ src/EventListener/AuthenticationSuccessHandler.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\EventListener;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class AuthenticationSuccessListener
+{
+    /**
+     * @param AuthenticationSuccessEvent $event
+     */
+    public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event)
+    {
+        $data = $event->getData();
+        $user = $event->getUser();
+
+        if (!$user instanceof UserInterface) {
+            return;
+        }
+
+        $data['user'] = [
+            'id' => $user->getId(),
+            'email' => $user->getEmail(),
+            'full_name' => $user->getFullName(),
+        ];
+
+        $event->setData($data);
+    }
+}
