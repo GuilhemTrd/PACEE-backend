3b55712c89e4d5c361fe4a601bbdef77db06db80
guilhemtrd <guilhem.trd@gmail.com>
2024-12-16 14:33:00 +0100

    🔧 Debug pipeline : ajout de l'injection sécurisée de JWT_PASSPHRASE dans le .env

--- src/Controller/ApiAuthController.php
+++ src/Controller/ApiAuthController.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Entity\User;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
